7a9f2f9c1d742547a53f38737b1fe2f3386f2e04
SAMSAM5.5 <sami.saubion@gmail.com>
2025-09-01 19:18:26 +0200

    Added premake5 script

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "freetype"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/*.h",
+        "src/*.c",
+    }
+
+    includedirs
+    {
+        "src",
+        "builds/unix",
+        "builds/windows",
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
